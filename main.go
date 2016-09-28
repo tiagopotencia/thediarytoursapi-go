8d3c7786e3ae64d486406925232799c6bac5f23a
tiagopotencia <tiagomourabrandao@gmail.com>
2016-09-28 05:54:40 -0300

    Remove endpoint / e coloca go:1.7 em app.json

--- main.go
+++ main.go
@@ -31,9 +31,6 @@ func main() {
 		ValidateHeaders: false,
 	}))
 
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
 	routes.SetRoutes(r)
 
 	r.Run(":" + port)
